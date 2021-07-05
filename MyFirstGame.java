1ca766d35e9c60b1d7435f58b41f28b128fdb0f5
drewandrewd <47378481+drewandrewd@users.noreply.github.com>
2021-07-06 00:17:03 +0500

    Create MyFirstGame.java

---
+++ MyFirstGame.java
@@ -0,0 +1,18 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 100);
+        int playerNumber = 50;
+        while (playerNumber != randomNumber) {
+
+            if (playerNumber > randomNumber) {
+                System.out.println("Данное число больше того, что загадал компьютер");
+                playerNumber--;
+            }
+            else {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+                playerNumber++;
+            }
+        }
+        System.out.println("Поздравляю, число угадано!");
+    }
+}
