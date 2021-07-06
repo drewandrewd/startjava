11b3dd81786f9a6d19e4303b42278da8fd8d8878
drewandrewd <47378481+drewandrewd@users.noreply.github.com>
2021-07-06 12:14:34 +0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
+public class Unicode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String sign = scanner.nextLine();
         int a = scanner.nextInt();
+        String sign = scanner.next();
         int b = scanner.nextInt();
         if (sign.equals("+")) {
             System.out.println(a + b);
-        }
-        else if (sign.equals("-")) {
+        } else if (sign.equals("-")) {
             System.out.println(a - b);
-        }
-        else if (sign.equals("*")) {
+        } else if (sign.equals("*")) {
             System.out.println(a * b);
-        }
-        else if (sign.equals("/")) {
+        } else if (sign.equals("/")) {
             System.out.println(a / b);
-        }
-        else if (sign.equals("^")) {
-            System.out.println(Math.pow(a, b));
-        }
-        else if (sign.equals("%")) {
+        } else if (sign.equals("^")) {
+            if (b >= 2) {
+                for (int i = 2; i <= b; i++) {
+                    a *= a;
+                }
+            } else if (b == 0) {
+                a = 1;
+            }
+            System.out.println(a);
+        } else if (sign.equals("%")) {
             System.out.println(a % b);
         }
     }
