cc68eb197d46c862f0dcea504b80506bfbb0841d
drewandrewd <andrewid10@gmail.com>
2021-08-02 20:29:54 +0500

    new

--- src/Lesson 1/Unicode.java
+++
@@ -1,9 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        int unicode = 9398;
-        while (unicode <= 10178) {
-            System.out.println((char) unicode);
-            unicode++;
-        }
-    }
-}
